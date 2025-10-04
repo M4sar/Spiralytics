0eecea9f14e2c8baff22a6084018826036930244
M4sar <ibragimov3366@gmail.com>
2025-10-04 18:08:46 +0500

    many adaptive

--- src/components/header/NavList.jsx
+++ src/components/header/NavList.jsx
@@ -3,8 +3,10 @@ import "./Header.css";
 export function NavList({ headerImg, headerText, imgClass, textClass }) {
   return (
     <li>
-      <img src={headerImg} className={imgClass} />
-      <p className={textClass}>{headerText}</p>
+      <a href="">
+        <img src={headerImg} className={imgClass} />
+        <p className={textClass}>{headerText}</p>
+      </a>
     </li>
   );
 }
